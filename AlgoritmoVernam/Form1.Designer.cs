dbf5e052444c7205936ae54f1cdb33478509697c
jyllatupa <chelsea_30_01@hotmail.com>
2024-07-05 22:51:45 -0500

    app algoritmo vernam en .net

---
+++ AlgoritmoVernam/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace AlgoritmoVernam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtpalabra = new System.Windows.Forms.TextBox();
+            this.txtclave = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.LBLCIFRADO = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ALGORITMO DE VERNAM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PALABRA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 118);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CLAVE";
+            // 
+            // txtpalabra
+            // 
+            this.txtpalabra.Location = new System.Drawing.Point(135, 64);
+            this.txtpalabra.Margin = new System.Windows.Forms.Padding(4);
+            this.txtpalabra.Name = "txtpalabra";
+            this.txtpalabra.Size = new System.Drawing.Size(586, 22);
+            this.txtpalabra.TabIndex = 3;
+            // 
+            // txtclave
+            // 
+            this.txtclave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtclave.Location = new System.Drawing.Point(135, 110);
+            this.txtclave.Margin = new System.Windows.Forms.Padding(4);
+            this.txtclave.Name = "txtclave";
+            this.txtclave.ReadOnly = true;
+            this.txtclave.Size = new System.Drawing.Size(586, 24);
+            this.txtclave.TabIndex = 4;
+            this.txtclave.Text = "M4ST3R";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(608, 169);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "LIMPIAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(462, 169);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "CIFRAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // LBLCIFRADO
+            // 
+            this.LBLCIFRADO.AutoSize = true;
+            this.LBLCIFRADO.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLCIFRADO.Location = new System.Drawing.Point(39, 35);
+            this.LBLCIFRADO.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBLCIFRADO.Name = "LBLCIFRADO";
+            this.LBLCIFRADO.Size = new System.Drawing.Size(0, 18);
+            this.LBLCIFRADO.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 37);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 18);
+            this.label5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(39, 76);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 18);
+            this.label6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(39, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 18);
+            this.label7.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(39, 69);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 18);
+            this.label8.TabIndex = 14;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(733, 99);
+            this.panel1.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 218);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(733, 111);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "LETRAS SEPARADAS";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 346);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(733, 100);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "INICIO DE CIFRADO";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.LBLCIFRADO);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(12, 463);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(733, 81);
+            this.groupBox3.TabIndex = 18;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "CIFRADO FINAL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 560);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtclave);
+            this.Controls.Add(this.txtpalabra);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtpalabra;
+        private System.Windows.Forms.TextBox txtclave;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label LBLCIFRADO;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ AlgoritmoVernam/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlgoritmoVernam
+{
+    public partial class Form1 : Form
+    {
+        String[] TablaNumeros = new string[10];
+        String[] TablaNumerosNumber = new string[10];
+
+        String[] TablaLetras = new string[26];
+        String[] TablaLetrasNumber = new string[26];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //--------------------------------INICIO SEPARAR PALABRA EN LETRAS---------------------------//
+            string PALABRA = txtpalabra.Text.ToUpper();
+            PALABRA = PALABRA.Replace(" ", "");
+            string cadena = "";
+
+            int LONGITUD_P = PALABRA.Length;
+            int chunkSize = 1;
+
+            String[] ArrayPalabra = new string[LONGITUD_P];
+
+            for (int a = 0; a < LONGITUD_P; a += chunkSize)
+            {
+                if (a + chunkSize > LONGITUD_P) chunkSize = LONGITUD_P - a;
+                cadena = cadena + (PALABRA.Substring(a, chunkSize)) + " - ";
+
+                ArrayPalabra[a] = PALABRA.Substring(a, chunkSize);
+            }
+
+            label5.Text = cadena.Remove(cadena.Length - 3).ToUpper();
+            //--------------------------------FIN SEPARAR PALABRA EN LETRAS---------------------------//
+
+
+            //--------------------------------INICIO SEPARAR CLAVE EN LETRAS---------------------------//
+            string CLAVE = txtclave.Text;
+            string cadena2 = "";
+            string new_clave = "";
+
+            int LONGITUD_C = CLAVE.Length;
+            int chunkSize2 = 1;
+
+            String[] ArrayClave = new string[0];
+
+            if (LONGITUD_C >= LONGITUD_P) //cuando la longitud de la clave es mayor igual a la longitud de la palabra
+            {
+                CLAVE = CLAVE.Substring(0, LONGITUD_P);
+
+                int LenCadena = CLAVE.Length;
+                ArrayClave = new string[LenCadena];
+
+                for (int c = 0; c < LenCadena; c += chunkSize2)
+                {
+                    if (c + chunkSize2 > LenCadena) chunkSize2 = LenCadena - c;
+                    cadena2 = cadena2 + (CLAVE.Substring(c, chunkSize2)) + " - ";
+
+                    ArrayClave[c] = CLAVE.Substring(c, chunkSize2);
+                }
+            }
+            else if (LONGITUD_C < LONGITUD_P)
+            {
+                int dif = LONGITUD_P - LONGITUD_C;
+                int len = LONGITUD_C + dif;
+
+                while (new_clave.Length <= len)
+                {
+                    new_clave += CLAVE;
+                }
+
+                new_clave = new_clave.Substring(0, len);
+                ArrayClave = new string[len];
+
+                for (int x = 0; x < len; x += chunkSize2)
+                {
+                    if (x + chunkSize2 > len) chunkSize2 = len - x;
+                    cadena2 = cadena2 + (new_clave.Substring(x, chunkSize2)) + " - ";
+                    ArrayClave[x] = new_clave.Substring(x, chunkSize2);
+                }
+            }
+
+            label6.Text = cadena2.Remove(cadena2.Length - 3).ToUpper();
+            //--------------------------------FIN SEPARAR CLAVE EN LETRAS---------------------------//
+
+            //CREANDO LA TABLA DE LETRAS
+            char letra = 'A';
+            int i = 0;
+
+            while (letra <= 'Z')
+            {
+                TablaLetras[i] = Convert.ToString(letra);
+                TablaLetrasNumber[i] = Convert.ToString(i);
+                i++;
+                letra++;
+            }
+            //--------------------------
+
+            //CREANDO LA TABLA DE NUMEROS
+            int number = 0;
+            int aux = 27;
+
+            while (aux <=36)
+            {
+                TablaNumeros[number] = Convert.ToString(number);
+                TablaNumerosNumber[number] = Convert.ToString(aux);
+                number++;
+                aux++;
+            }
+            //--------------------------
+
+
+            //-----------------------INICIO DE CIFRADO-------------------------------------
+            string searchLetra = "";
+            string searchClave = "";
+            string dato = "";
+            string cifrado_palabra = "";
+            string cifrado_clave = "";
+
+            for (int b = 0; b < LONGITUD_P; b += chunkSize)
+            {
+                //CIFRANDO LA PALABRA-----------------------------------------------------
+                searchLetra = ArrayPalabra[b];
+
+                if (isNumeric(searchLetra))
+                {
+                    int index = Array.IndexOf(TablaNumeros, searchLetra);
+                    dato = TablaNumerosNumber[index];
+                }
+                else {
+                    int index = Array.IndexOf(TablaLetras, searchLetra);
+                    dato = TablaLetrasNumber[index];
+                }
+
+                cifrado_palabra = cifrado_palabra + searchLetra + "="+ dato + " - ";
+                //-----------------------------------------------------------------------
+
+
+
+                //CIFRANDO LA CLAVE-----------------------------------------------------
+                searchClave = ArrayClave[b];
+
+                if (isNumeric(searchClave))
+                {
+                    int index = Array.IndexOf(TablaNumeros, searchClave);
+                    dato = TablaNumerosNumber[index];
+                }
+                else
+                {
+                    int index = Array.IndexOf(TablaLetras, searchClave);
+                    dato = TablaLetrasNumber[index];
+                }
+
+                cifrado_clave = cifrado_clave + searchClave + "=" + dato + " - ";
+                //-----------------------------------------------------------------------
+            }
+
+            label7.Text = cifrado_palabra.Remove(cifrado_palabra.Length - 3).ToUpper().Replace("-"," ");
+
+            label8.Text = cifrado_clave.Remove(cifrado_clave.Length - 3).ToUpper().Replace("-", " ");
+
+            CifradoFinal(cifrado_palabra, cifrado_clave, LONGITUD_P);
+            //-----------------------FIN DE CIFRADO-------------------------------------
+        }
+
+        public static Boolean isNumeric(String cadena)
+        {
+            Boolean resultado;
+
+            try
+            {
+                int.Parse(cadena);
+                resultado = true;
+            }
+            catch (Exception e)
+            {
+                resultado = false;
+            }
+
+            return resultado;
+        }
+
+        void CifradoFinal(string cifrado_palabra, string cifrado_clave, int longitud)
+        {
+            string[] palabra = cifrado_palabra.Split('-');
+            string[] clave = cifrado_clave.Split('-');
+            string[] dato;
+            string[] dato2;
+            int aux = 0;
+            int aux2 = 0;
+            int total = 0;
+            string cifrado = "";
+            string resultado = "";
+
+            for (int i = 0; i < longitud; i++)
+            {
+                dato = palabra[i].Split('=');
+                aux = Convert.ToInt32(dato[1]);
+
+                dato2 = clave[i].Split('=');
+                aux2 = Convert.ToInt32(dato2[1]);
+
+                total = aux + aux2;
+
+                if (total < 37)
+                {
+                    resultado = BuscarValor(total);
+                }
+                else {
+                    total = total % 37;
+
+                    resultado = BuscarValor(total);
+                }
+
+                cifrado = cifrado + resultado + "   ";
+            }
+
+            LBLCIFRADO.Text = cifrado.ToUpper();
+        }
+
+        string BuscarValor(int total)
+        {
+            string cifrado = "";
+            int index = 0;
+
+            if (total <= 25)
+            {
+                cifrado = TablaLetras[total];
+            }
+            else {
+                index = Array.IndexOf(TablaNumerosNumber, Convert.ToString(total));
+                cifrado = TablaNumeros[index];
+            }
+
+            return cifrado;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtpalabra.Clear();
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            LBLCIFRADO.Text = "";
+        }
+    }
+}
+
+
